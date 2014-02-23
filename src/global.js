833b5e51627b06c0853ad4b5ce30227b0ff61c49
Mathias Kretschek <mathias@kretschek.com.br>
2014-02-22 23:22:39 -0300

    Added a module for getting a reference for the global object.

---
+++ src/global.js
@@ -0,0 +1,5 @@
+define(function () {
+  'use strict';
+  // Gets a reference to the global object.
+  return Function('return this')();
+});
---
+++ test/global.js
@@ -0,0 +1,17 @@
+define([
+  'src/global'
+], function (
+  global
+) {
+  var currentGlobal = this;
+
+  describe('global', function () {
+    it('is an object', function () {
+      expect(global).to.be.an('object');
+    });
+
+    it('is the global object', function () {
+      expect(global).to.equal(currentGlobal);
+    });
+  });
+});
